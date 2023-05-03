b2bfdb97c7396ee894024f103ba3305d7af67d35
Shivam Varshney <uietcse1923shivamvarshney@kuk.ac.in>
2023-05-03 15:34:29 +0530

    516

---
+++ Medium/516.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int longestPalindromeSubseq(string s) {
+        int n = s.size();
+        vector<vector<int>> dp(n,vector<int>(n,0));
+        for(int gap = 1 ; gap <= n ; gap++){
+            int i = 0;
+            int j = i+gap-1;
+            while(j < n){
+                if(gap == 1){
+                    dp[i][j] = 1;
+                }else{
+                    dp[i][j] = s[i]==s[j]?2:1;
+                }else{
+                    dp[i][j] = ( s[i]==s[j] ? 2+dp[i+1][j-1] : max(dp[i][j-1],dp[i+1][j]) );
+                }
+                j++;
+                i++;
+            }
+        }
+        return dp[0][n-1];
+    }
+};
