40b3671cfd0f52b2116c2816767c237073771ab5
Shivam Varshney <uietcse1923shivamvarshney@kuk.ac.in>
2023-05-03 15:33:11 +0530

    LCS

--- ExtraQuestions/LongestCommonSubsequence.cpp
+++ ExtraQuestions/LongestCommonSubsequence.cpp
@@ -3,14 +3,27 @@ using namespace std;
 
 vector<vector<int>> dp;
 
-int lcs(string a,string b,int i,int j){
-	if(i < 0 || j < 0) return 0;
-	if(a[i] == b[j]) return 1+lcs(i-1,j-1);
-	return max(lcs(i-1,j),lcs(i,j-1));
+int lcs(string &a,string &b,int n,int m){
+    if(n == 0 || m == 0) return dp[n][m] = 0;
+    if(dp[n][m] != -1) return dp[n][m];
+    if(a[n-1] == b[m-1]) return dp[n][m] = 1+lcs(a,b,n-1,m-1);
+    return dp[n][m] = max(lcs(a,b,n-1,m),lcs(a,b,n,m-1));
 }
 
-int lcs_tab(string s){
-	
+int lcs_tab(string &a,string &b){
+	int N = a.size();
+    int M = b.size();
+    for(int n = 0 ; n <= N ; n++){
+        for(int m = 0 ; m <= M ; m++){
+            if(n == 0 || m == 0) {
+                dp[n][m] = 0;
+                continue;
+            }
+            if(a[n-1] == b[m-1]) dp[n][m] = 1+dp[n-1][m-1];
+            else dp[n][m] = max(dp[n-1][m],dp[n][m-1]);
+        }
+    }
+    return dp[N][M];
 }
 
 int main(){
@@ -22,7 +35,7 @@ int main(){
 	string a,b;cin>>a>>b;
 	int n = a.size();
 	int m = b.size();
-	dp.resize(n,vector<int>(m,-1));
-	cout<<lcs(a,b,n-1,m-1)<<"\n";
+	dp.resize(n+1,vector<int>(m+1,-1));
+	cout<<lcs(a,b,n,m)<<"\n";
 	return 0;
 }
