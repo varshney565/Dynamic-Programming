7b78c000f333e2cd6cdd7374246b7b5d43073618
Shivam Varshney <uietcse1923shivamvarshney@kuk.ac.in>
2023-05-08 18:11:06 +0530

    EditDIstanceFollowUp

---
+++ ExtraQuestions/Edit_Distance_follow_up.cpp
@@ -0,0 +1,28 @@
+/**
+ * question is the extension of edit distance.
+ * 
+ * here the cost is also given.
+ * for inserting the cost is a.
+ * for deleting the cost is b.
+ * for replace the cost is c.
+ * 
+ * 
+ * now find the minimum cost to convert from s to t using minimum cost.
+ * 
+ * 
+ * 
+ * 
+ * 
+ * sol : 
+ * 
+ * 
+ * as inserting in one string is equal to deleting from another string.
+ * 
+ * so we will take the cost of insert and delete same which will be equal to min(insert , delete)
+ * 
+ * 
+ * 
+*/
+
+
+
