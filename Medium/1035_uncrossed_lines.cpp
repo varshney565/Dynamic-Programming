fbafb5b2fd41beff67e33cd07002624bab439ecb
Shivam Varshney <uietcse1923shivamvarshney@kuk.ac.in>
2023-05-22 15:09:18 +0530

    1035

---
+++ Medium/1035_uncrossed_lines.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxUncrossedLines(vector<int>& nums1, vector<int>& nums2) {
+        int n = nums1.size();
+        int m = nums2.size();
+        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
+        //find the LCS
+        function<int(int,int)> go = [&](int n,int m){
+            if(n == 0 || m == 0) return dp[n][m] = 0;
+            if(dp[n][m] != -1) return dp[n][m];
+            if(nums1[n-1] == nums2[m-1]) return dp[n][m] = 1+go(n-1,m-1);
+            return dp[n][m] = max(go(n-1,m),go(n,m-1));
+        };
+        return go(n,m);
+    }
+};
