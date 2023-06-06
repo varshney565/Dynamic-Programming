fc41fe4bad243df7ef30bb1d40ffbbfd4652111c
Shivam Varshney <uietcse1923shivamvarshney@kuk.ac.in>
2023-06-06 14:53:14 +0530

    number_of_solutions_of_equation

---
+++ ExtraQuestions/find_number_of_solutions_of_a_linear_equation_of_n_variable.cpp
@@ -0,0 +1,11 @@
+/***
+ * 
+ * find number of solutions of a linear equation with n variables.
+ * 
+ * 
+ * coff = {1,2,3,4} , c = 6
+ * 
+ * 
+ * it is just the unbounded knacksack i can take the coff any number of times >= 0.
+ * 
+ * */
