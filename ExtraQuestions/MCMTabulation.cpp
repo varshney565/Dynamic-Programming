3c80d0ce1ccdffa5291f48675b693d3f73a507a2
Shivam Varshney <uietcse1923shivamvarshney@kuk.ac.in>
2023-06-14 18:22:47 +0530

    MCMtab

--- ExtraQuestions/MCMTabulation.cpp
+++ ExtraQuestions/MCMTabulation.cpp
@@ -1,16 +1,46 @@
-class Solution {
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
 public:
-    int maxCoins(vector<int>& nums) {
-        int n = nums.size();
-        vector<vector<int>> dp(n,vector<int>(n,0));
-        for(int gap = 1 ; gap <= n ; gap++){
-            for(int i = 0,j = gap-1 ; j < n ; i++,j++){
-                int lf = (i-1 >= 0 ? nums[i-1] : 1),rf = (j+1 < n ? nums[j+1] : 1);
-                for(int k = i ; k <= j ; k++){
-                    dp[i][j] = max(dp[i][j],(i <= k-1 ? dp[i][k-1] : 0)+(k+1 <= j ? dp[k+1][j] : 0)+lf*nums[k]*rf);
+    int matrixMultiplication(int n, int arr[]){
+        vector<vector<int>> dp(n,vector<int>(n,INT_MAX));
+        for(int st = n-1 ; st >= 0 ; st--){
+            for(int en = 0 ; en < n ; en++){
+                if(en - st == 1) {
+                    dp[st][en] = 0;
+                    continue;
+                }
+                for(int cut = st + 1 ; cut < en ; cut++){
+                    dp[st][en] = min(dp[st][en],dp[st][cut]+dp[cut][en]+arr[st]*arr[cut]*arr[en]);
                 }
             }
         }
         return dp[0][n-1];
     }
 };
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        int arr[N];
+        for(int i = 0;i < N;i++)
+            cin>>arr[i];
+        
+        Solution ob;
+        cout<<ob.matrixMultiplication(N, arr)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
